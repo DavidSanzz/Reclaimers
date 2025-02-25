417053ca84bed221bf17845537c6bef645ead5e3
David Sanz <sanzsierradavid@gmail.com>
2025-02-25 20:16:59 +0100

    Agregada plantilla Spring Boot para backend

---
+++ api-reclaimers/src/main/java/com/reclaimers/api_reclaimers/ApiReclaimersApplication.java
@@ -0,0 +1,13 @@
+package com.reclaimers.api_reclaimers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReclaimersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReclaimersApplication.class, args);
+	}
+
+}
---
+++ api-reclaimers/src/test/java/com/reclaimers/api_reclaimers/ApiReclaimersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reclaimers.api_reclaimers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReclaimersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
