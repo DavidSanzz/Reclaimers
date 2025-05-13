6329f49a47835d15c7619573baea9474efba0498
David Sanz <sanzsierradavid@gmail.com>
2025-05-13 13:15:49 +0200

    Modificaciones en el Controlador y Repositorio de Usuariopara obtener listas por tipo usuario

--- api-reclaimers/src/main/java/com/reclaimers/api_reclaimers/repositorios/RepositorioUsuario.java
+++ api-reclaimers/src/main/java/com/reclaimers/api_reclaimers/repositorios/RepositorioUsuario.java
@@ -3,6 +3,8 @@ package com.reclaimers.api_reclaimers.repositorios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.reclaimers.api_reclaimers.models.Usuario;
 
+import java.util.List;
+
 public interface RepositorioUsuario extends JpaRepository<Usuario, Long> {
 
     // Método para verificar si el email ya existe
@@ -10,4 +12,7 @@ public interface RepositorioUsuario extends JpaRepository<Usuario, Long> {
 
     // Método para encontrar usuario por email
     Usuario findByEmail(String email);
+
+    List<Usuario> findByTipoUsuario(Usuario.TipoUsuario tipoUsuario);
+
 }
